d39ed140de8bc7aa9565a55b394b003dcd773ac7
Matt Koopman <mkoopman1@studentmail.nait.ca>
2015-11-24 16:53:53 -0700

    sECURITY

---
+++ eRestaurantDemo/eRestaurantSystem/DAL/Security/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity.EntityFramework;
+using eRestaurantSystem.DAL.Entities.Security;
+#endregion
+
+namespace eRestaurantSystem.DAL.Security
+{
+    internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
---
+++ eRestaurantDemo/eRestaurantWebsite/UserControls/DateTimeMocker.ascx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional Namespaces
+using eRestaurantSystem.BLL;
+#endregion
+
+public partial class UserControls_DateTimeMocker : System.Web.UI.UserControl
+{
+    //create properties that will allow external users of this control to have access to the data 
+    //(date and time) on this control.
+    public DateTime MockDate 
+    {
+        get
+        {
+            //setup a variable to hold the date
+            //this variable will be initialized to a default
+            DateTime date = DateTime.MinValue;
+
+            //possibly override the default date with the contents of the web control Search date
+            DateTime.TryParse(SearchDate.Text , out date);
+            //return date value
+            return date;
+        }
+        set
+        {
+            SearchDate.Text = value.ToString("yyyy-mm-dd");
+        }
+    }
+
+    public TimeSpan MockTime
+    {
+        get
+        {
+            //setup a variable to hold the date
+            //this variable will be initialized to a default
+            TimeSpan time = TimeSpan.MinValue;
+
+            //possibly override the default date with the contents of the web control Search date
+            TimeSpan.TryParse(SearchTime.Text, out time);
+            //return date value
+            return time;
+        }
+        set
+        {
+            SearchTime.Text = DateTime.Today.Add(value).ToString("DefaultHttpHandler:mm:ss");
+        }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void MockLastBillingDateTime_Click(object sender, EventArgs e)
+    {
+        AdminController sysmgr = new AdminController();
+        DateTime info = sysmgr.GetLastBillDateTime();
+        SearchDate.Text = info.ToString("yyyy-MM-dd");
+        SearchTime.Text = info.ToString("HH:mm");
+
+    }
+}
